6423d61919b960c6aa3b7c50aac3d8ef63c96203
unknown <soryung11@naver.com>
2025-07-24 16:06:07 +0900

    Update national_pension.py

--- national_pension.py
+++ national_pension.py
@@ -71,10 +71,10 @@ class PensionData():
     def get_data(self):
         return self.df
 
-@st.cache
+@st.cache_data
 def read_pensiondata():
-    # data = PensionData('https://www.dropbox.com/s/nxeo1tziv05ejz7/national-pension.csv?dl=1')
-    data = PensionData('national-pension.csv')
+    url = 'https://www.dropbox.com/s/nxeo1tziv05ejz7/national-pension.csv?dl=1'
+    data = PensionData(url)
     return data
 
 data = read_pensiondata()
